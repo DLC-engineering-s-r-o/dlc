c8e3f2e357000f557da1f75afdb847105f72c7f5
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-03-02 14:49:40 +0100

    priject init

---
+++ Resources/Scripts/init.ts
@@ -0,0 +1,9 @@
+﻿import { initNav } from "./navigation";
+import { initAnim } from "./boxAnim";
+import { setCurentYear } from "./year";
+
+document.addEventListener("DOMContentLoaded", function () {
+  initNav();
+  initAnim();
+  setCurentYear();
+});
