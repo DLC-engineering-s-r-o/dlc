9f3c04824c08e8559eebc53bd23b4678e3309346
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-03-29 12:56:01 +0200

    mail url fix

--- Resources/Scripts/email.ts
+++ Resources/Scripts/email.ts
@@ -35,7 +35,7 @@ export function initEmail() {
         setEmailContent()
 
         if (fullName != '' && email != '' && message != '') {
-            window.location.href = `https://dlcafsendgrid20220328153355.azurewebsites.net/api/SendEmail?sender=${fullName}&email=${email}&phone=${phone}&body=${message}&website=${website}`
+            window.location.href = `https://dlcafsendgrid20220328153355.azurewebsites.net/api/SendEmail?fullName=${fullName}&email=${email}&phone=${phone}&body=${message}&website=${website}`
         }
 
         else console.log('%cPlease fill in the required fields!', 'color:red;')
