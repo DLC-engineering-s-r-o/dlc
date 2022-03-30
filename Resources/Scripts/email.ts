780f07b3547dcca72d9834915b82496e4af8d684
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-03-30 09:42:42 +0200

    mail form updated

--- Resources/Scripts/email.ts
+++ Resources/Scripts/email.ts
@@ -1,9 +1,16 @@
+import axios from 'axios'
+
 export function initEmail() {
+    axios as any
+
     const fullNameEl = document.getElementById('fullName') as HTMLInputElement
     const phoneEl = document.getElementById('phone') as HTMLInputElement
     const mailEl = document.getElementById('email') as HTMLInputElement
     const messageEl = document.getElementById('msg') as HTMLTextAreaElement
     const submitBtn = document.getElementById('submitBtn') as HTMLButtonElement
+    const formValidation = document.getElementById('formValidation') as HTMLDivElement
+
+    const azureFncUrl = 'https://dlcafsendgrid20220328153355.azurewebsites.net/api/SendEmail'
 
     let fullName = ''
     let email = ''
@@ -11,16 +18,27 @@ export function initEmail() {
     let message = ''
     let website = 'DLC management'
 
-    function setEmailContent() {
+    function validateEmail(mail) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+            return true
+        }
+        console.log("%cYou have entered an invalid email address!", 'color:red;')
+        return false
+    }
 
+    function showSuccess(){
+        formValidation.style.display = 'block'
+    }
+
+    function setEmailContent() {
         if (fullNameEl.value) {
             fullName = fullNameEl.value
         }
         if (phoneEl.value) {
-            phone = phoneEl.value
+            phone = phoneEl.value.replace('+', '00')
         }
 
-        if (mailEl.value) {
+        if (validateEmail(mailEl.value)) {
             email = mailEl.value
         }
 
@@ -31,13 +49,40 @@ export function initEmail() {
         return fullName + phone + email + message
     }
 
+    function axiosPost() {
+        let formData = new FormData()
+
+        formData.append('fullName', fullName)
+        formData.append('email', email)
+        formData.append('phone', phone)
+        formData.append('body', message)
+        formData.append('website', website)
+
+        axios({
+            method: "post",
+            url: azureFncUrl,
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" },
+          })
+            .then(response => {
+             showSuccess()
+              console.log(response);
+            })
+            .catch(response => {
+
+              console.log(response);
+            });
+    }
+
     submitBtn.addEventListener('click', () => {
         setEmailContent()
 
         if (fullName != '' && email != '' && message != '') {
-            window.location.href = `https://dlcafsendgrid20220328153355.azurewebsites.net/api/SendEmail?fullName=${fullName}&email=${email}&phone=${phone}&body=${message}&website=${website}`
+            axiosPost()
         }
 
         else console.log('%cPlease fill in the required fields!', 'color:red;')
     })
+
+
 }
